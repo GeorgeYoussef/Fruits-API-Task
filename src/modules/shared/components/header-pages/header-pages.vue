204b3060ce446f7db06fe6577d4a528149d1eff6
George Mikhaeil <george.youssef222@gmail.com>
2024-12-16 20:03:43 +0000

    push modules

---
+++ src/modules/shared/components/header-pages/header-pages.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="conOfHeaderPages">
+    <div class="sectionHeader mianTitleResposive">
+      <span> {{ headerPage.part1 }} </span> {{ headerPage.part2 }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["headerPage"],
+};
+</script>
+<style scoped lang="scss">
+@import "~@/assets/styles/abstracts/variables";
+.conOfHeaderPages {
+  .sectionHeader {
+    font-size: 2.5rem;
+    color: $color-secondary;
+    font-weight: bold;
+    text-transform: capitalize;
+    span {
+      color: $color-primary;
+    }
+  }
+}
+</style>
---
+++ src/modules/shared/model/apiModel.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apiModel: {
+    status: 0,
+    message: "",
+    data: "",
+    error: "",
+  },
+  requestType: {
+    add: 1,
+    delete: 4,
+    error: 10,
+  },
+};
